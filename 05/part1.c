182544a40ce490d5199f1c5902eba4136d43b16b
irek <irek@gabr.pl>
2023-12-05 07:09:50 +0100

    FIRST TRYYYYYYY

---
+++ 05/part1.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef unsigned long id_t;
+
+struct map {
+	id_t src, dst, len;
+	struct map *next;       /* Linked list FTW!!! (^-^ ) */
+};
+
+static struct map *map_parse(void)
+{
+	struct map *head = 0;
+	struct map *node = 0;
+	char buf[BUFSIZ], *bp;
+	fgets(buf, sizeof(buf), stdin);
+	while ((bp = fgets(buf, sizeof(buf), stdin))) {
+		if (buf[0] == '\n') {
+			break;
+		}
+		if (node) {
+			node->next = malloc(sizeof(struct map));
+			node = node->next;
+		} else {
+			node = malloc(sizeof(struct map));
+			head = node;
+		}
+		node->dst = atoi(bp);
+		bp = strchr(bp, ' ');
+		node->src = atoi(++bp);
+		bp = strchr(bp, ' ');
+		node->len = atoi(++bp);
+	}
+	return head;
+}
+
+static id_t map_get(struct map *map, id_t id)
+{
+	struct map *node;
+	for (node = map; node; node = node->next) {
+		if (id < node->src || id >= node->src + node->len) {
+			continue;
+		}
+		return node->dst + (id - node->src);
+	}
+	return id;
+}
+
+
+int main(void)
+{
+	struct map *seed_soil;
+	struct map *soil_fertilizer;
+	struct map *fertilizer_water;
+	struct map *water_light;
+	struct map *light_temperature;
+	struct map *temperature_humidity;
+	struct map *humidity_location;
+	id_t id, seeds[32];
+	id_t location = -1; /* Force integer overflow to init with biggest value. */
+	size_t i, seeds_siz = 0;
+	char buf[BUFSIZ], *bp;
+	/* seeds */
+	bp = fgets(buf, sizeof(buf), stdin);
+	while ((bp = strchr(bp, ' '))) {
+		seeds[seeds_siz++] = atoi(++bp);
+	}
+	fgets(buf, sizeof(buf), stdin);
+	seed_soil            = map_parse();
+	soil_fertilizer      = map_parse();
+	fertilizer_water     = map_parse();
+	water_light          = map_parse();
+	light_temperature    = map_parse();
+	temperature_humidity = map_parse();
+	humidity_location    = map_parse();
+	for (i = 0; i < seeds_siz; i++) {
+		id = map_get(seed_soil, seeds[i]);
+		id = map_get(soil_fertilizer, id);
+		id = map_get(fertilizer_water, id);
+		id = map_get(water_light, id);
+		id = map_get(light_temperature, id);
+		id = map_get(temperature_humidity, id);
+		id = map_get(humidity_location, id);
+		if (id < location) {
+			location = id;
+		}
+	}
+	printf("%lu\n", location);
+	return 0;
+}
